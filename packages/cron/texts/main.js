bb8c1ff9f0ed370548bd1818ffe8b99d1ea3768d
Tofik Khan <tofikahmad@live.com>
2022-12-22 23:00:16 -0800

    Connect to Database

--- packages/cron/texts/main.js
+++ packages/cron/texts/main.js
@@ -1,6 +1,33 @@
-exports.main = (args) => {
-    let name = args.name || process.env.name || "TEST";
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body": greeting}
+const {MongoClient} = require('mongodb');
+
+exports.main = async () => {
+    console.log("...Initiating Cron...");
+    await sendNotification();
+    return {"body": "Done!"}
 }
+
+
+async function sendNotification() {
+    let secretaries = await getAllSecretaries();
+    console.log(secretaries);
+  }
+  
+  async function getAllSecretaries() {
+    const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
+  
+      const client = new MongoClient(uri);
+   
+      try {
+          await client.connect();
+   
+          return await client
+                          .db("waqfenau")
+                          .collection('secretaries')
+                          .find({})
+                          .toArray(); 
+      } catch (e) {
+          console.error(e);
+      } finally {
+          await client.close();
+      }
+  }
